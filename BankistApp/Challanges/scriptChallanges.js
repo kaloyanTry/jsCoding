8d859450f6e77242518d48823b691e9e323eb34d
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2022-02-06 11:27:04 +0200

    challanges
    course chalanges

---
+++ BankistApp/Challanges/scriptChallanges.js
@@ -0,0 +1,22 @@
+const data1 = [5, 2, 4, 1, 15, 8, 3];
+const data2 = [16, 6, 10, 5, 6, 1, 4];
+
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? age * 2 : 16 + age * 4));
+  console.log(humanAges);
+
+  // Filter humanAges > 18:
+  const adults = humanAges.filter(age => age >= 18);
+  console.log(adults);
+
+  // const averageAge = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+  // return averageAge;
+  // Full reduce method use case:
+  const avrg = adults.reduce((acc, age, _i, arr) => acc + age / arr.length, 0);
+  return avrg;
+};
+
+const calc1 = calcAverageHumanAge(data1).toFixed(2);
+const calc2 = calcAverageHumanAge(data2).toFixed(2);
+
+console.log(calc1, calc2);
