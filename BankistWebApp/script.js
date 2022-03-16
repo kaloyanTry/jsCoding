d1a9a37d19d5feb09fe29a813ef017281d6fc82a
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2022-03-16 21:34:43 +0200

    Add files via upload

--- BankistWebApp/script.js
+++ BankistWebApp/script.js
@@ -75,6 +75,26 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 
 headerObserver.observe(header);
 
+// Reveal sections:
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 ////////////////////////////////////////////////////////////////////
 // Exercises:
 // Selecting DOM elements:
@@ -83,10 +103,10 @@ headerObserver.observe(header);
 // console.log(document.body);
 
 // Select section => Node list:
-const allSections = document.querySelectorAll('.section');
+// const allSections = document.querySelectorAll('.section');
 
-// Select => HtmlCollection = live collection:
-const allButtons = document.getElementsByTagName('button');
+// // Select => HtmlCollection = live collection:
+// const allButtons = document.getElementsByTagName('button');
 //console.log(document.getElementsByClassName('btn'));
 
 ////////////////////////////////////////////////////////////////////////
